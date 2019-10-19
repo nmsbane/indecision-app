acf671219c3febff86b9d5c5fad411938dc9a3fe
madhusudan <madhusudhan638@gmail.com>
2019-10-19 23:24:54 +0530

    setup babel to watch for changes inside src/app.js file and automatically generate the compiled version inside public/scripts directory, which index.html refers, can see the command to run babel inside src/app.js file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
-"use strict";
-
 console.log("app.js is running");
 
 // JSX -Javascript XML
 // var template = React.createElement("p", null, "This is JSX from app.js");
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
