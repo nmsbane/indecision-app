da1c7582edb73f7d5cb870c6b4885af5386ce217
madhusudan <madhusudhan638@gmail.com>
2019-10-19 22:37:47 +0530

    bare bones of a react app, convert jsx into javascript code using babel, use reactjs by including the files

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log("app.js is running");
+
+// JSX -Javascript XML
+var template = React.createElement("p", null, "This is JSX from app.js");
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
