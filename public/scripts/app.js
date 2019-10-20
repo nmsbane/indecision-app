8a2aeefda5f73b0c53755303430c33403567f7d3
madhusudan <madhusudhan638@gmail.com>
2019-10-20 22:27:18 +0530

    using jsx expression to construct html, and script/app.js will be automatically filled by babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,55 @@ console.log("app.js is running");
 // JSX -Javascript XML
 // var template = React.createElement("p", null, "This is JSX from app.js");
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision App"
+  React.createElement(
+    "h1",
+    null,
+    "Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
+);
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Awesome bane"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "I am learning React.js and Node.js"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "I have to finish them before going to new job"
+  )
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
 
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ console.log("app.js is running");
 
 // JSX -Javascript XML
 // var template = React.createElement("p", null, "This is JSX from app.js");
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var template2 = (
+  <div>
+    <h1>Awesome bane</h1>
+    <p>I am learning React.js and Node.js</p>
+    <p>I have to finish them before going to new job</p>
+  </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
 
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
