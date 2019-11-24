8d14a2efd448d9dedd12ee7c11123c076906a329
madhusudan <madhusudhan638@gmail.com>
2019-11-24 16:13:13 +0530

    style the buttons to reflect the mockup of indecision app, note: created a new componenet file _button.scss, which holds styles related to the buttons

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,7 +3,11 @@ import React from "react";
 const Action = props => {
   return (
     <div>
-      <button disabled={!props.hasOptions} onClick={props.handlePick}>
+      <button
+        className="big-button"
+        disabled={!props.hasOptions}
+        onClick={props.handlePick}
+      >
         What should i do?
       </button>
     </div>
