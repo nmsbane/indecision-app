7d1d7484e68fcfe0c1595a2a116554fa50969cf5
madhusudan <madhusudhan638@gmail.com>
2019-11-24 17:19:43 +0530

    style the modal using flexbox property, also learned how to style 3rd party component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,16 @@ const OptionModal = props => {
       isOpen={!!props.selectedOption}
       contentLabel="selected option"
       onRequestClose={props.clearSelectedOption}
+      closeTimeoutMS={200}
+      className="modal"
     >
-      <h3>Selected option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.clearSelectedOption}>Okay</button>
+      <h3 className="modal__title">Selected option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button" onClick={props.clearSelectedOption}>
+        Okay
+      </button>
     </Modal>
   );
 };
